8b4ba4f471d52bca995930d7dd42716296aa27b0
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-06-25 22:20:15 +0200

    Create app.py
    our streamlit classifier

---
+++ app.py
@@ -0,0 +1,106 @@
+"""
+    Simple Streamlit webserver application for serving developed classification
+	models.
+    Author: Explore Data Science Academy.
+    Note:
+    ---------------------------------------------------------------------
+    Plase follow the instructions provided within the README.md file
+    located within this directory for guidance on how to use this script
+    correctly.
+    ---------------------------------------------------------------------
+    Description: This file is used to launch a minimal streamlit web
+	application. You are expected to extend the functionality of this script
+	as part of your predict project.
+	For further help with the Streamlit framework, see:
+	https://docs.streamlit.io/en/latest/
+"""
+
+# Streamlit dependencies
+import streamlit as st
+import joblib,os
+
+# general
+import numpy as np 
+import pandas as pd
+import pickle
+
+# text preprocessing
+import re
+from string import punctuation
+import nltk
+nltk.download(['stopwords','punkt'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# models
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.svm import LinearSVC
+
+# metrics
+from sklearn.metrics import classification_report
+
+# function to preprocess tweets
+def preprocess(tweet):
+  tweet = tweet.lower() # converting everything to lower case
+  tweet = re.sub(r"\W", " ", tweet)
+  tweet = re.sub(r'#([^\s]+)', r'\1', tweet) 
+  tweet = word_tokenize(tweet) # tokenizing the tweets so that they may be used in modeling and/or NLP
+  stopwords_list = set(stopwords.words('english') + list(punctuation)) # cresting a list containing punctuation and stop words
+  tweets = [word for word in tweet if word not in stopwords_list] # iterating over the list and saving the output into a list 
+  return " ".join(tweet)
+
+# The main function where we will build the actual app
+def main():
+	"""Tweet Classifier App with Streamlit """
+
+	# Creates a main title and subheader on your page -
+	# these are static across all pages
+	st.title("Tweet Classifer")
+	st.subheader("Climate change tweet classification")
+
+	# Creating sidebar with selection box -
+	# you can create multiple pages this way
+	options = ["Home", "About","Modelling"]
+	selection = st.sidebar.selectbox("Choose Option", options)
+
+	# Building out the "Information" page
+	if selection == "About":
+		st.info("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+	    # add EDA
+	    # add real world research
+
+		st.subheader("Raw Twitter data and label")
+		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			st.write("show raw data") # will write the df to the page
+
+	# Building out the predication page
+	if selection == "Home":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+
+		if st.button("Classify Linear SVC model"):
+			tweet = tweet_text
+			print("input tweet : \n {}".format(tweet))
+			tweet_processed = preprocess(tweet)
+			print("processed tweet : \n {}".format(tweet_processed))
+			tweet_pred = unpickled_model.predict([tweet_processed])
+			print("predicted",tweet_pred)
+			st.success("Text Categorized as: {}".format(tweet_pred))
+
+	# add choose a model button
+
+# Required to let Streamlit instantiate our web app.  
+if __name__ == '__main__':
+    print('loading pickle file')
+    model_load_path = "resources/linear_SVC.pkl"
+    
+    with open(model_load_path,'rb') as file:
+        unpickled_model = pickle.load(file)
+
+    print("model successfully pickled")
+    main()
