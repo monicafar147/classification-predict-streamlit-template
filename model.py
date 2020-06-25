2d6ec3f8a6be1ed2c37786175454c7945e317426
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-06-25 21:55:40 +0200

    Update model.py
    tested normal text in model.py

--- model.py
+++ model.py
@@ -1,37 +1,34 @@
-# Helper Dependencies
-import numpy as np
+# general
+import numpy as np 
 import pandas as pd
-pd.options.mode.chained_assignment = None 
 import pickle
+import joblib,os
 
-
-# cleaning text
+# text preprocessing
 import re
+from string import punctuation
+import nltk
+nltk.download(['stopwords','punkt'])
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from string import punctuation 
-from nltk.corpus import stopwords 
-#nltk.download('stopwords')
-#nltk.download('wordnet')
-from sklearn.model_selection import train_test_split
 
-# Machine learning models
-from sklearn.feature_extraction.text import TfidfVectorizer
+# models
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from sklearn.svm import LinearSVC
 
 # metrics
-from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import classification_report
 
-
-def _preprocess(tweet):
-    stopwords_list = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
-    tweet = tweet.lower() # convert text to lower-case
-    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) # remove URLs
-    tweet = re.sub(r"\W", " ", tweet) # remove usernames
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet) # remove the # in #hashtag
-    tweet = word_tokenize(tweet) # remove repeated characters (helloooooooo into hello)
-    tweets = [word for word in tweet if word not in stopwords_list]
-    return " ".join(tweets) 
+def preprocess(tweet):
+  tweet = tweet.lower() # converting everything to lower case
+  tweet = re.sub(r"\W", " ", tweet)
+  tweet = re.sub(r'#([^\s]+)', r'\1', tweet) 
+  tweet = word_tokenize(tweet) # tokenizing the tweets so that they may be used in modeling and/or NLP
+  stopwords_list = set(stopwords.words('english') + list(punctuation)) # cresting a list containing punctuation and stop words
+  tweets = [word for word in tweet if word not in stopwords_list] # iterating over the list and saving the output into a list 
+  return " ".join(tweet)
 
 if __name__ == '__main__':
 
@@ -39,7 +36,7 @@ if __name__ == '__main__':
     print('loading in data')
     train = pd.read_csv('https://raw.githubusercontent.com/monicafar147/classification-predict-streamlit-template/master/climate-change-belief-analysis/train.csv')
     print(train.head())
-    train['processed'] = train['message'].apply(_preprocess)
+    train['processed'] = train['message'].apply(preprocess)
     print('data preprocessed')
     #print(train.head())
 
@@ -49,39 +46,43 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state =10)
 
     # apply model on train data
-    # Linear SVC:
+    #creating a pipeline with the tfid vectorizer and a linear svc model
     svc = Pipeline([('tfidf',TfidfVectorizer()),('classify',LinearSVC())])
+
+    #fitting the model
     svc.fit(X_train, y_train)
 
     #apply model on test data
-    pred = svc.predict(X_test)
+    y_pred_svc = svc.predict(X_test)
 
     # Print a classification report
     print('report on y_test vs svc.predict(X_test)')
-    print(classification_report(y_test, pred))
+    print(classification_report(y_test, y_pred_svc))
 
     # pickle model
-    #model_save_path = "r"esources/svc.pkl"svc.pkl"
-    #with open(model_save_path,'wb') as file:
-    #    pickle.dump(svc,file)
+    model_save_path = "resources/linear_SVC.pkl"
+    with open(model_save_path,'wb') as file:
+        pickle.dump(svc,file)
 
     print('loading pickle file')
-    model_load_path = "resources/svc.pkl"
+    model_load_path = "resources/linear_SVC.pkl"
     
     with open(model_load_path,'rb') as file:
         unpickled_model = pickle.load(file)
 
     # load test data in
     test = pd.read_csv('https://raw.githubusercontent.com/monicafar147/classification-predict-streamlit-template/master/climate-change-belief-analysis/test.csv')
-    test['processed'] = test['message'].apply(_preprocess)
+    test['processed'] = test['message'].apply(preprocess)
     X_unseen = test['processed']
 
-    print('model loaded: True')
     y_unseen = unpickled_model.predict(X_unseen)
+    print('model loaded: True')
     print('y_unseen')
     print(y_unseen)
 
-    submit_dict = {'tweetid': test['tweetid'], 'tweet':X_unseen,'sentiment':y_unseen}
-    submit_df = pd.DataFrame(data=submit_dict)
-    submit_df.set_index('tweetid')
-    submit_df.to_csv("kaggle.csv",index  = False)
+    tweet = "china is to blame for climate change! #die #flood"
+    new = preprocess(tweet)
+    print(new)
+    tweet_pred = unpickled_model.predict([new])
+    print("predicted",tweet_pred)
+
