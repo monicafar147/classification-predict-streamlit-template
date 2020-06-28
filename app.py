542eba332536f22f35250f2a576be7c7020c605c
Keane Byrne <frontorange@gmail.com>
2020-06-28 18:29:29 +0200

    changed heading to correct heading
    Logistic Model - Linear SVC Model

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def main():
 			st.write("show raw data") # will write the df to the page
 
 	if selection == "Modelling":
-		st.subheader("Logistic Regression Model")
+		st.subheader("Linear SVC Model")
 		st.text("Logistic regression is a supervised learning classification algorithm") 
 		st.text("used to predict the probability of a target variable.") 
 		st.text("This model works best on binary data classification but almost performs well in our data") 
